416183b9796fe5a7c904e16646aab942671528f9
LucasTaboada <ltlucastaboada@gmail.com>
2018-08-24 22:54:37 -0400

    added js hot reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
+var $ = require('gulp-load-plugins')({lazy:true});
 
 
 
 gulp.task('sass', function(){
     return gulp
             .src('./src/sass/**/*.scss')
-            .pipe(sass().on('error',sass.logError))
+            .pipe($.sass().on('error',$.sass.logError))
             .pipe(gulp.dest('public/css'))
             .pipe(browserSync.reload({stream:true}))
 })
@@ -25,16 +25,26 @@ gulp.task('browser-sync',['sass'],function(){
         }
     })
 })
+gulp.task('scripts',function(){
+    return gulp
+    .src(['./src/js/**/*.js'])
+    .pipe($.plumber())
+    .pipe($.babel())
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.reload({stream:true}));
+})
 gulp.task('watch', function(){
      gulp.watch('src/sass/**/*.scss',['sass',browserSync.reload]);
      gulp.watch('src/**/*.html',['html',browserSync.reload]);
      gulp.watch('public/*.html').on('change',browserSync.reload);
+     gulp.watch('src/js/**/*.js',['scripts',browserSync.reload]);
      
 })
 gulp.task('default', function(){
     gulp.start(
         'sass',
         'html',
+        'scripts',
         'browser-sync',
         'watch'
         
